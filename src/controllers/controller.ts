a71612ea4e9091c990fae95985d93bddef4a0909
Thiago-M-Silva <thiagobossun06@gmail.com>
2024-08-27 19:28:26 -0300

    Inicio do projeto PS (processo seletivo)

---
+++ src/controllers/controller.ts
@@ -0,0 +1,11 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { ServiceClass } from "../services/service";
+
+
+class Controller {
+
+}
+
+export { Controller }
+
+// 28:59 - Criando API completa com NODEJS + TypeScript e MongoDB (sujeito programador)
